8ffc204e1b1055b6c7f8eed314acb6623809b28a
Baldan789 <btnsnmurun@gmail.com>
2024-10-24 16:05:13 +0800

    Add files via upload

---
+++ Bodlogo4.m
@@ -0,0 +1,28 @@
+% A bodlogo
+a = 1 + 3i;
+b = 1 - 3i;
+result_A = a / b;
+disp('A hariu:');
+disp(result_A);
+
+% B nodlogo
+result_B = exp(1i * pi / 4);
+disp('B hariu:');
+disp(result_B);
+
+% C bodlogo 
+a = 5 + 4i;
+b = 4 - 2i;
+sum_C = a + b;
+product_C = a * b;
+disp('C niilber: ');
+disp(sum_C);
+disp('C hesgiin urjver: ');
+disp(product_C);
+
+% D bodlogo 
+numerator = 6 + 6i;
+denominator = 2 + 1i;
+result_D = numerator / denominator;
+disp('D hariu: ');
+disp(result_D);
